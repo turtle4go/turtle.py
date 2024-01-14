9addfd536ba9062ab4d76088b24b2eaa421d3b2e
turtle4go <108996967+turtle4go@users.noreply.github.com>
2024-01-13 20:09:23 -0500

    Commit main.py
    main script

---
+++ main.py
@@ -0,0 +1,55 @@
+import time
+
+print("hi this is turtle")
+print("i will make you say stuff")
+print("and if you dont i will kick you out")
+time.sleep(3)
+input = input("say dogecoin:")
+if input == "dogecoin":
+  print("cool")
+  print("now say turtle4go is the best")
+  unput = input("say it:")
+  if unput == "turtle4go is the best":
+    print("good kid you are")
+    print("guess what")
+    print("only a few people make it here")
+    print("now NO people make it after this! muwhahahahahhahahwhwhhaahahhwhwhahah!!!!!!")
+    time.sleep(1.5)
+    print("oh my laugh wasnt as dramtic as i thought it was lol")
+    onput = input("say 87370454165387563478563478564875634 divded by 87342 and since i have a calculator built inside me i can outsmart you HAHAHAHAHHA:")
+    if onput == 87370454165387563478563478564875634/87342:
+      print("WHATATATTATATATATTAATTATATATATTATAT!?!?!??! H--- O-O-OO---- W-wWww--W!?!?!??!??!")
+      print("oh maybe u cheated and used an online calculator oof")
+      print("but you shall be stabbed to the heart in this one HAHAHAHHAAHAHAH!!!!!!!")
+      time.sleep(2.5)
+      enput = input("say i am a sussy baka dun dun dun dun:")
+      if enput == "i am a sussy baka dun dun dun dun":
+        print("HA you are a sussy baka you said it yourself LOL!")
+        print("this last one is the hardest and tuffest one")
+        time.sleep(5)
+        anput = input("what is skibidi toilet episode 59 about in one word MUWHAHAHAHHAHAHAHA!!!!:")
+        if anput == "titan-speakerman":
+          print("my time has come")
+          print("i will get my revenge....")
+          time.sleep(2.5)
+          print("you be like: BRUH IDC WITH THE SAD LORE!")
+          print("U WIN!!!!")
+        else:
+          print("get wrecked")
+          time.sleep(3)
+          break
+      else:
+        print("rick astley got you and rickrolled you so fly high")
+        time.sleep(1.5)
+        break
+    else:
+      print("get vanished to adoms kid")
+      time.sleep(1)
+      break
+  else:
+    print("THEE SHALL BE BANISHED!")
+    time.sleep(3)
+else:
+  print("die")
+  time.sleep(3)
+  break
